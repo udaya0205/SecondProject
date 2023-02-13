9aa59183735e5798c629ff334da9cb46b7842019
udaya0205 <125043102+udaya0205@users.noreply.github.com>
2023-02-13 16:38:04 +0530

    Update Dropdowndemo.java

--- src/test/java/com/automatiom/project/Dropdowndemo.java
+++ src/test/java/com/automatiom/project/Dropdowndemo.java
@@ -10,8 +10,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class Dropdowndemo {
 
 	public static void main(String[] args) throws InterruptedException  {
-		
-	
+			
 	WebDriverManager.chromedriver().setup();
 	ChromeDriver driver = new ChromeDriver();
 	driver.manage().window().maximize();
@@ -28,12 +27,6 @@ public class Dropdowndemo {
     Thread.sleep(2000);
     driver.close();
     
-    
-    
-
-		
-		
-		
-	}
+    }
 
 }
